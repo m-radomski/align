9e8895eb181f1902769af1a5f520d339ebfaca79
Mateusz Radomski <radomski.main@protonmail.com>
2021-09-09 18:39:43 +0200

    Stop removal of lines that do not have eq on them

--- align.zig
+++ align.zig
@@ -3,42 +3,49 @@ const std = @import("std");
 const Kilobyte = 1024;
 const Megabyte = Kilobyte * 1024;
 
-const EqlLine = struct {
-  startWhitespace: []const u8,
-  line: []const u8,
-  eqlPos: usize,
+const Line = struct {
+    startWhitespace: []const u8,
+    line: []const u8,
+    eqlPos: ?usize,
 };
 
 pub fn main() anyerror!void {
-  var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-  var allocator = &arena.allocator;
-  defer arena.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var allocator = &arena.allocator;
+    defer arena.deinit();
 
-  var stdinInput = try std.io.getStdIn().reader().readAllAlloc(allocator, 1000 * Megabyte);
+    var stdinInput = try std.io.getStdIn().reader().readAllAlloc(allocator, 1000 * Megabyte);
 
-  var eqlLines = std.ArrayList(EqlLine).init(allocator);
-  var linesIt = std.mem.split(u8, stdinInput, "\n");
+    var lines = std.ArrayList(Line).init(allocator);
+    var linesIt = std.mem.split(u8, stdinInput, "\n");
 
-  var maxEqlPos: usize = 0;
-  while(linesIt.next()) | line | {
-    var trimmedLine = std.mem.trimLeft(u8, line, &std.ascii.spaces);
-    var whitespace = line[0..line.len - trimmedLine.len];
+    var maxEqlPos: usize = 0;
+    while (linesIt.next()) |line| {
+        var trimmedLine = std.mem.trimLeft(u8, line, &std.ascii.spaces);
+        var whitespace = line[0 .. line.len - trimmedLine.len];
 
-    if(std.mem.indexOf(u8, trimmedLine, "=")) | eqlPos | {
-      try eqlLines.append(.{ .startWhitespace = whitespace, .line = trimmedLine, .eqlPos = eqlPos });
-      maxEqlPos = std.math.max(maxEqlPos, eqlPos);
+        var insertLine: Line = .{ .startWhitespace = whitespace, .line = trimmedLine, .eqlPos = null };
+
+        if (std.mem.indexOf(u8, trimmedLine, "=")) |eqlPos| {
+            insertLine.eqlPos = eqlPos;
+            maxEqlPos = std.math.max(maxEqlPos, eqlPos);
+        }
+
+        try lines.append(insertLine);
     }
-  }
 
-  const spaces = " "**128;
+    const spaces = " " ** 128;
 
-  for(eqlLines.items) | eqlLine | {
-    // std.log.debug("Line {d}[{d}]:{s}", .{ eqlLine.eqlPos, maxEqlPos, eqlLine.line });
-    std.debug.print("{s}{s}{s}{s}\n", .{
-        eqlLine.startWhitespace,
-        eqlLine.line[0..eqlLine.eqlPos],
-        spaces[0..(maxEqlPos-eqlLine.eqlPos)],
-        eqlLine.line[eqlLine.eqlPos..],
-    });
-  }
+    for (lines.items) |line| {
+        if (line.eqlPos) |eqlPos| {
+            std.debug.print("{s}{s}{s}{s}\n", .{
+                line.startWhitespace,
+                line.line[0..eqlPos],
+                spaces[0..(maxEqlPos - eqlPos)],
+                line.line[eqlPos..],
+            });
+        } else {
+          std.debug.print("{s}{s}\n", .{ line.startWhitespace, line.line });
+        }
+    }
 }
